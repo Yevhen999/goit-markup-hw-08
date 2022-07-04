963904f2487cc28358d07a3792b518eb919336ee
Yevhen <leshchenkoyevhen@gmail.com>
2022-07-04 20:13:08 +0300

    add header

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.button-menu__open'),
+    closeMenuBtn: document.querySelector('.button-menu__close'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
